cc05e99a40d1627e70b7c9526759d99a8a37c707
MRMIDO770 <164278364+MRMIDO770@users.noreply.github.com>
2024-06-01 00:32:42 +0300

    RDB.sh

--- RDB.sh
+++ RDB.sh
@@ -1,4 +1,4 @@
-wget -O ng.sh https://raw.githubusercontent.com/hhammouch/phordp/master/ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://github.com/MRMIDO770/rbdd/blob/main/RDB.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 
